c2f300b5e1113e9be61d9d2eac11c1a1077c31b8
ranigopi <68233604+ranigopi@users.noreply.github.com>
2020-09-11 22:00:25 +0530

    Add files via upload

---
+++ Day 6 Assignment B7.py
@@ -0,0 +1,33 @@
+import math
+class cone:
+	def _init_(self,radius,height):
+		self.radius=radius
+		self.height=height
+		
+def volume(self):
+	print("enter radius",self.radius)
+	print("enter height",self.height)
+	print("volume =",math.pi*(self.radius**2)+( self.height/3))
+	
+def area(self):
+	print("enter radius",self.radius)
+	print("enter height",self.height)
+	print("surface area",math.pi*(self.radius**2))
+	print("base",math.pi*self.height*sqrt(self.radius**2+self.height**2))
+	
+volume(self)
+area(self)
+
+class bank():
+	def _init_(self,owner,balance):
+		self.owner=owner
+		self.balance=balance
+	def deposist(self):
+		if deposist<balance:
+			print("withdrawl",self.balance)
+	def withdraw(self):
+		if withdraw>balance:
+			print("over",self.balance)
+				
+deposist()
+withdraw()
